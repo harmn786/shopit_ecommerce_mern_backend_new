1bcf152f3e2363991bac9099dfc43690b912f1c0
Abdul Rehman <91444728+harmn786@users.noreply.github.com>
2025-09-26 23:02:04 +0500

    fix content

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,22 +1,12 @@
-// export default (user,statusCode,res)=>{
-//     const token = user.getJwtToken()
-//     const options = {
-//         expires:new Date(Date.now()+process.env.COOKIE_EXPIRE_TIME * 24 * 60 * 60 * 1000),
-//         httpOnly:true,
-//          sameSite: "none",  // important for cross-site cookies
-//   secure: true       // required for HTTPS on Vercel
-//     }
-//     res.status(statusCode).cookie("token",token,options).json({
-//         token,
-//     })
-// }
-
-export default (user, statusCode, res) => {
-  const token = user.getJwtToken();
-
-  res.status(statusCode).json({
-    success: true,
-    token,
-    user,
-  });
-};
+export default (user,statusCode,res)=>{
+    const token = user.getJwtToken()
+    const options = {
+        expires:new Date(Date.now()+process.env.COOKIE_EXPIRE_TIME * 24 * 60 * 60 * 1000),
+        httpOnly:true,
+         sameSite: "none",  // important for cross-site cookies
+  secure: true       // required for HTTPS on Vercel
+    }
+    res.status(statusCode).cookie("token",token,options).json({
+        token,
+    })
+}
