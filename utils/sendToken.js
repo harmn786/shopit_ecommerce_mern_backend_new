76039e9b4d1722be81e8625754bcef9ace0dd553
Abdul Rehman <91444728+harmn786@users.noreply.github.com>
2025-09-26 21:53:55 +0500

    Update sendToken.js

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -2,7 +2,9 @@ export default (user,statusCode,res)=>{
     const token = user.getJwtToken()
     const options = {
         expires:new Date(Date.now()+process.env.COOKIE_EXPIRE_TIME * 24 * 60 * 60 * 1000),
-        httpOnly:true
+        httpOnly:true,
+         sameSite: "none",  // important for cross-site cookies
+          secure: true       // required for HTTPS on Vercel
     }
     res.status(statusCode).cookie("token",token,options).json({
         token,
