2dbdd2c52b7567b7cfd42d3c1e8a4fd92597a3a7
Abdul Rehman <91444728+harmn786@users.noreply.github.com>
2025-09-26 22:50:18 +0500

    fix credentials true

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,14 +4,27 @@ import User from '../models/User.js';
 import ErrorHandler from '../utils/errorHandler.js';
 export const isAuthenticatedUser = catchAsyncErrors(async (req,res,next)=>{
 
-    const {token} = req.cookies
-    if(!token){
-        return next(new ErrorHandler("Please login to access this resource",401))
-    }
+    // const {token} = req.cookies
+    // if(!token){
+    //     return next(new ErrorHandler("Please login to access this resource",401))
+    // }
 
-    const decoded  = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id);
-    next();
+    // const decoded  = jwt.verify(token, process.env.JWT_SECRET);
+    // req.user = await User.findById(decoded.id);
+    // next();
+     let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return next(new ErrorHandler("Not authorized", 401));
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = await User.findById(decoded.id);
+  next();
 })
 export const authorizeRoles = (...roles) => {
     return (req,res,next)=>{
