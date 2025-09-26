c9bde69a36e4115a202ad017776c814f4f6df356
Abdul Rehman <91444728+harmn786@users.noreply.github.com>
2025-09-26 23:12:16 +0500

    modify sendToken file

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,12 +1,21 @@
-export default (user,statusCode,res)=>{
-    const token = user.getJwtToken()
-    const options = {
-        expires:new Date(Date.now()+process.env.COOKIE_EXPIRE_TIME * 24 * 60 * 60 * 1000),
-        httpOnly:true,
-         sameSite: "none",  // important for cross-site cookies
-  secure: process.env.NODE_ENV === "production" // secure only in prod
-    }
-    res.status(statusCode).cookie("token",token,options).json({
-        token,
-    })
-}
+// export default (user,statusCode,res)=>{
+//     const token = user.getJwtToken()
+//     const options = {
+//         expires:new Date(Date.now()+process.env.COOKIE_EXPIRE_TIME * 24 * 60 * 60 * 1000),
+//         httpOnly:true,
+//          sameSite: "none",  // important for cross-site cookies
+//   secure: process.env.NODE_ENV === "production" // secure only in prod
+//     }
+//     res.status(statusCode).cookie("token",token,options).json({
+//         token,
+//     })
+// }
+export default (user, statusCode, res) => {
+  const token = user.getJwtToken();
+
+  res.status(statusCode).json({
+    success: true,
+    token,
+    user,
+  });
+};
